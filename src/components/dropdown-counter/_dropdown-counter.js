ffdb7ed72d2278e0b7c61e54cb0bec27606c0837
ViktoriaMagina <victoria.magina@yandex.ru>
2022-03-17 22:39:54 +0300

    закончена разработка dropdown-counter

--- src/components/dropdown-counter/_dropdown-counter.js
+++ src/components/dropdown-counter/_dropdown-counter.js
@@ -1,7 +1,9 @@
 const dropdownСounterHeaders = document.querySelectorAll(".dropdown-counter__header");
 const dropdownСounterMinus = document.querySelectorAll("[data-counter-minus]");
 const dropdownСounterPlus = document.querySelectorAll("[data-counter-plus]");
-
+const dropdownСounterBtnClear = document.querySelectorAll("[data-counter-clear]")
+const dropdownСounterBtnSelect = document.querySelectorAll("[data-counter-select]")
+let dropdownСounterHeaderNum = 0;
     
 
 dropdownСounterHeaders.forEach((header)=> {
@@ -20,23 +22,65 @@ dropdownСounterHeaders.forEach((header)=> {
 })
 
 dropdownСounterMinus.forEach((item)=> {
+    const dropdownСounter = item.closest(".dropdown-counter")
     const dropdownСounterBox = item.closest(".dropdown-counter__box")
+    const dropdownСounterHeader = dropdownСounter.querySelector(".dropdown-counter__header")
     const dropdownСounterNum = dropdownСounterBox.querySelector("[data-counter-num]")
-    const dropdownСounterBtnClear = dropdownСounterBox.querySelector("[data-counter-clear]")
-    item.addEventListener("click", function () {
-        if(dropdownСounterNum.innerText < 20)
+    const dropdownСounterBtnClear = dropdownСounter.querySelector("[data-counter-clear]")
+    item.addEventListener("click", function() {
+        if(dropdownСounterNum.innerText < 20){
             dropdownСounterNum.innerText = parseInt(dropdownСounterNum.innerText) + 1
+            dropdownСounterHeaderNum += 1
+            if(dropdownСounterHeaderNum == 1)
+                dropdownСounterHeader.value = `${dropdownСounterHeaderNum} гость`
+            if(dropdownСounterHeaderNum < 5 & dropdownСounterHeaderNum !=1)
+                dropdownСounterHeader.value = `${dropdownСounterHeaderNum} гостя`
+            if(dropdownСounterHeaderNum >= 5 )
+                dropdownСounterHeader.value = `${dropdownСounterHeaderNum} гостей`
+        }
+        if(dropdownСounterHeaderNum == 0)
+            dropdownСounterHeader.value = ""
         parseInt(dropdownСounterNum.innerHTML) != 0 ? dropdownСounterBtnClear.classList.add("active"):dropdownСounterBtnClear.classList.remove("active")
     })
 })
 dropdownСounterPlus.forEach((item)=> {
+    const dropdownСounter = item.closest(".dropdown-counter")
     const dropdownСounterBox = item.closest(".dropdown-counter__box")
+    const dropdownСounterHeader = dropdownСounter.querySelector(".dropdown-counter__header")
     const dropdownСounterNum = dropdownСounterBox.querySelector("[data-counter-num]")
-    const dropdownСounterBtnClear = dropdownСounterBox.querySelector("[data-counter-clear]")
+    const dropdownСounterBtnClear = dropdownСounter.querySelector("[data-counter-clear]")
     item.addEventListener("click", function () {
-        if(dropdownСounterNum.innerText > 0)
+        if(dropdownСounterNum.innerText > 0){
             dropdownСounterNum.innerText = parseInt(dropdownСounterNum.innerText) - 1
-        parseInt(dropdownСounterNum.innerHTML) != 0 ? dropdownСounterBtnClear.classList.add("active"):dropdownСounterBtnClear.classList.remove("active")
+            dropdownСounterHeaderNum = dropdownСounterHeaderNum - 1
+            if(dropdownСounterHeaderNum == 1)
+                dropdownСounterHeader.value = `${dropdownСounterHeaderNum} гость`
+            if(dropdownСounterHeaderNum < 5 & dropdownСounterHeaderNum !=1)
+                dropdownСounterHeader.value = `${dropdownСounterHeaderNum} гостя`
+            if(dropdownСounterHeaderNum >= 5 )
+                dropdownСounterHeader.value = `${dropdownСounterHeaderNum} гостей`
+        }
+        if(dropdownСounterHeaderNum == 0)
+            dropdownСounterHeader.value = ""
+        parseInt(dropdownСounterNum.innerText) != 0 ? dropdownСounterBtnClear.classList.add("active"):dropdownСounterBtnClear.classList.remove("active")
     })
 })
 
+dropdownСounterBtnClear.forEach((item) => {
+    const dropdownСounter = item.closest(".dropdown-counter")
+    const dropdownСounterNums = dropdownСounter.querySelectorAll("[data-counter-num]")
+    item.addEventListener("click", function(){
+        dropdownСounterNums.forEach((obj)=> {
+            obj.innerText = 0;
+        })
+    })
+})
+
+dropdownСounterBtnSelect.forEach((item) => {
+    const dropdownСounter = item.closest(".dropdown-counter")
+    const dropdownСounterBody = dropdownСounter.querySelector(".dropdown-counter__body")
+    item.addEventListener("click", function(){
+        dropdownСounterBody.classList.remove("active");
+        dropdownСounter.classList.remove("active")
+    })
+})
