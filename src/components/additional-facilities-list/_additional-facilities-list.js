6004385f372ccfa064c0c7aa764fdac7ad3508be
ViktoriaMagina <victoria.magina@yandex.ru>
2022-03-21 13:37:42 +0300

    добавлен компонент additional-facilities-list

---
+++ src/components/additional-facilities-list/_additional-facilities-list.js
@@ -0,0 +1,17 @@
+const additionalFacilitiesListTitles = document.querySelectorAll('.additional-facilities-list__title')
+
+additionalFacilitiesListTitles.forEach((header)=> {
+    header.addEventListener("click", function() {
+        const additionalFacilitiesList = this.closest(".additional-facilities-list");
+        const additionalFacilitiesListTitle = additionalFacilitiesList.querySelector(".additional-facilities-list__title")
+        const additionalFacilitiesListBody =  additionalFacilitiesList.querySelector(".additional-facilities-list__body");
+        additionalFacilitiesListBody.classList.toggle("active");
+        additionalFacilitiesListTitle.classList.toggle("active");
+        document.addEventListener("click", function(e){
+            if(!(e.target).closest(".additional-facilities-list")){
+                additionalFacilitiesListBody.classList.remove("active");
+                additionalFacilitiesListTitle.classList.remove("active")
+            }
+        })
+    })
+})
