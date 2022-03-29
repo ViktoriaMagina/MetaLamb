8caffe2e4531ac8f1a8b6b15033f0c9ffce64ee2
ViktoriaMagina <victoria.magina@yandex.ru>
2022-03-29 18:51:26 +0300

    пофиксила неправильную работу слайдера на странице search-room

--- src/components/room-item/_room-item.js
+++ src/components/room-item/_room-item.js
@@ -1,12 +1,39 @@
-import * as $ from 'jquery'
-import 'slick-carousel'
+// import * as $ from 'jquery'
+// import 'slick-carousel'
 
-$(".room-item__slider").slick({
-    infinite: false,
-    slidesToShow: 1,
-    lidesToShow: 1,
-    dots: true,
-    fade: true,
-    nextArrow: '<button type="button" class="room-item__slider-next"></button">',
-    prevArrow: '<button type="button" class="room-item__slider-prev"></button>',
-});
+// $(".room-item__slider").slick({
+//     infinite: false,
+//     slidesToShow: 1,
+//     lidesToShow: 1,
+//     dots: true,
+//     fade: true,
+//     variableWidth: true,
+//     centerMode: true,
+//     nextArrow: '<button type="button" class="room-item__slider-next"></button">',
+//     prevArrow: '<button type="button" class="room-item__slider-prev"></button>',
+// });
+
+
+import Swiper from 'swiper/bundle';
+
+
+
+const swiper = new Swiper('.swiper', {
+    // slidesPerView: 2,
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        // 900: {
+        //   slidesPerView: 1,
+        // },
+    }
+  });
+  
