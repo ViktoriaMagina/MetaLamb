c9d4e11b41ad75208380d1b63a3f13b3dff9fb39
ViktoriaMagina <victoria.magina@yandex.ru>
2022-04-14 22:27:25 +0300

    небольшие изменения в модальных окнах и меню

--- src/components/find-numbers-form/_find-numbers-form.js
+++
@@ -1,2 +0,0 @@
-import ScrollReveal from 'scrollreveal'
-ScrollReveal().reveal('.find-numbers-form', { delay: 300 }, { easing: 'cubic-bezier(.78,.77,.4,.4) ' },{ mobile: false });
--- src/components/room-impressions/_room-impressions.js
+++ src/components/room-impressions/_room-impressions.js
@@ -1,6 +1,6 @@
 import ScrollReveal from 'scrollreveal'
-ScrollReveal().reveal('.room-impressions__circle-item-fabulous', { delay: 300 },{ opacity: .4 },{ mobile: false });
-ScrollReveal().reveal('.room-impressions__circle-item-good', { delay: 700 },{ opacity: .4 },{ mobile: false });
-ScrollReveal().reveal('.room-impressions__circle-item-satisfactorily', { delay: 1100 },{ opacity: .4 },{ mobile: false });
-ScrollReveal().reveal('.room-impressions__circle-item-disappointed', { delay: 1500 },{ opacity: .4 },{ mobile: false });
-ScrollReveal().reveal('.room-impressions__pieContainer', { delay: 1900 },{ opacity: .4 },{ mobile: false });
+ScrollReveal().reveal('.room-impressions__pieContainer', { delay: 300 },{ opacity: .4 },{ mobile: false });
+ScrollReveal().reveal('.room-impressions__circle-item-fabulous', { delay: 700 },{ opacity: .4 },{ mobile: false });
+ScrollReveal().reveal('.room-impressions__circle-item-good', { delay: 1100},{ opacity: .4 },{ mobile: false });
+ScrollReveal().reveal('.room-impressions__circle-item-satisfactorily', { delay: 1500 },{ opacity: .4 },{ mobile: false });
+ScrollReveal().reveal('.room-impressions__circle-item-disappointed', { delay: 1900 },{ opacity: .4 },{ mobile: false });
